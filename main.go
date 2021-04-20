465b7fabe6a9366e478d2e10b448a38d44f93c7a
mggk <mggk@protonmail.ch>
2021-04-20 21:19:02 +0200

    Update to use latest go version

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 	"regexp"
 )
 
-var g Game.Games
+var g Games
 var rd string
 
 func main() {
@@ -34,7 +34,7 @@ func main() {
 
 func scanRoms() {
 	g = nil
-	libRegEx, e := regexp.Compile("^.+\\.(bin)$")
+	libRegEx, e := regexp.Compile(`^.+\\.(bin)$`)
 	if e != nil {
 		log.Fatal(e)
 	}
